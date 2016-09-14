4d97d0d619e82b405575a39022db29ca6b0fc315
thingscouldbeworse <megal33tdude@gmail.com>
2016-09-14 12:12:22 -0400

    added directory for con logs

--- main.php
+++ main.php
@@ -2,7 +2,22 @@
 
 require( 'libs/CoderSitefuncs.php' );
 
-refreshNew( 'commtv_dummy' );
+$dbname = 'commtv_dummy';
 
+$blue 	= "\033[34m";
+$green 	= "\033[32m";
+$Cyan 	="\033[36m";
+$Red 	= "\033[31m";
+$Purple = "\033[35m";
+$Brown 	= "\033[33m";
+$lCyan 	= "\033[36m";
+$Yellow = "\033[33m";
+$off 	= "\033[0m";
+
+$search_hash = retrieveSearchHash( $dbname ); // get the search associated with the site
+
+$search_results_json = runCurlSearch( $search_hash, 50, 0 );
+
+print_r( $search_results_json->results->hits );
 
 ?>
