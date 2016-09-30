5780bd6a63107618ca376b6dda7d72ae97d66f77
thingscouldbeworse <megal33tdude@gmail.com>
2016-09-30 12:42:15 -0400

    changed dumb naming of CoderSiteFuncs from CoderSitefuncs and added in misc files again

--- main.php
+++ main.php
@@ -1,23 +1,9 @@
 <?php
-
 require( 'libs/CoderSitefuncs.php' );
 
-$dbname = 'commtv_dummy';
+// this file is run through a cron-job at 3 AM each day to refresh each coding site specified in 'commtv_refresh'
 
-$blue 	= "\033[34m";
-$green 	= "\033[32m";
-$Cyan 	="\033[36m";
-$Red 	= "\033[31m";
-$Purple = "\033[35m";
-$Brown 	= "\033[33m";
-$lCyan 	= "\033[36m";
-$Yellow = "\033[33m";
-$off 	= "\033[0m";
+refreshNew( 'commtv_testing', 1 );
 
-$search_hash = retrieveSearchHash( $dbname ); // get the search associated with the site
-
-$search_results_json = runCurlSearch( $search_hash, 50, 0 );
-
-print_r( $search_results_json->results->hits );
 
 ?>
